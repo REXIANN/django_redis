6f5a86443511e626063d6a85e9e7893feb705e65
김진혁 <gandolkim@gmail.com>
2021-01-03 12:53:58 +0900

    database migrated

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTiemField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Comment(models.Model):

0157686b0086c34e5f22bdbbc7629fe99280030f
김진혁 <gandolkim@gmail.com>
2021-01-05 23:02:50 +0900

    accounts logic added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .forms import CustomUserCreationForm
 # Create your views here.
+def login(request):
+    context = {
+        'form': CustomUserCreationForm
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def signup(request):
+    pass
+
+
+def logout(request):    
+    pass
--- django_redis/settings.py
+++ django_redis/settings.py
@@ -126,3 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# STATICFILES_DIRS = [
+#     STATIC_URL,
+#     '/templates/'
+# ]

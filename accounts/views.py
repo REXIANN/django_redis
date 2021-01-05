0ae997ea4d50ed8e4b7b29d30f507cc51e9e8f2b
김진혁 <gandolkim@gmail.com>
2021-01-05 23:23:40 +0900

    accounts signup logic added

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as user_login
+from django.contrib.auth import logout as user_logout
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+
 # Create your views here.
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
     context = {
-        'form': CustomUserCreationForm
+        'form': form
     }
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/form.html', context)
 
 
 def signup(request):
-    pass
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
 
 
+@login_required
 def logout(request):    
     pass
